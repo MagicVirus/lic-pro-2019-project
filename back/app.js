d022a9c2e76b64339e578557d0651fbcba0ba2db
Denis KARUPOVIC <d.karupovic@hotmail.com>
2019-01-09 21:55:07 +0100

    Added create route in api

--- back/app.js
+++ back/app.js
@@ -1,16 +1,49 @@
 var express = require('express');
 var db = require('./db/db');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
 // Set up the express app
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // get all todos
-app.get('/api/v1/list', (req, res) => {
+app.get('/episodes', (req, res) => {
     res.status(200).send({
         success: 'true',
         message: 'series retrieved successfully',
         todos: db
     })
+});
+app.post('/create', (req, res) => {
+  if(!req.body.name) {
+    return res.status(400).send({
+      body: req.body,
+      success: 'false',
+      message: 'name is required'
+    });
+  } else if(!req.body.code) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'code is required'
+    });
+  }
+  const serie = {
+    id: db.data.length + 1,
+    name: req.body.name,
+    code: req.body.code,
+    note: 0,
+  }
+  db.data.push(serie);
+  fs.writeFile('myjsonfile.json', JSON.stringify(db), 'utf8', null);
+
+  return res.status(201).send({
+    success: 'true',
+    message: 'serie added successfully',
+    serie
+  })
 });
 
 const PORT = 5000;
