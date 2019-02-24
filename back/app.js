5a5d3b327d87946c3bc85acf8fa671fddff4df74
Denis KARUPOVIC <d.karupovic@hotmail.com>
2019-02-24 21:01:44 +0100

    ISSAM pull AVANT de commit stp

--- back/app.js
+++ back/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fileManager = require('./fileManager');
 const app = express();
+const cors = require('cors');
 var fs = require('fs');
 
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
