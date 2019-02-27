912555689a5c0f1b484f7f121b24cdf85998c4eb
Julien Marcon <julgamer1@gmail.com>
2019-02-27 20:07:19 +0100

    Ajout de test pour la route get et post

--- back/test/test.js
+++ back/test/test.js
@@ -2,8 +2,9 @@ const assert = require('assert');
 const expect = require('chai').expect
 const request = require('supertest');
 const app = require('../app')
+const fs = require('fs');
 
-describe('Unit testing the /api/episodes route', function() {
+describe('Unit testing the GET /api/episodes route', function() {
 
     it('should return OK status', function() {
         return request(app)
@@ -12,5 +13,49 @@ describe('Unit testing the /api/episodes route', function() {
                 assert.equal(response.status, 200)
             })
     });
-
 });
+describe('Unit testing the GET /api/episodes', function() {
+    it('should return message on rendering', function() {
+        return request(app)
+            .get('/api/episodes')
+            .then(function(response){
+                expect(response.text).to.contain('episodes retrieved successfully');
+            })
+    });
+});
+describe('Unit testing the POST /api/episodes', function() {
+    it('should return OK status', function() {
+        return request(app)
+            .post('/api/episodes')
+            .send('name=test')
+            .send('code=test')
+            .send('note=01')
+            .then(function(response){
+                assert.equal(response.status, 200)
+            })
+    });
+});
+describe('Unit testing the POST /api/episodes', function() {
+    it('should return message on rendering', function() {
+        return request(app)
+            .post('/api/episodes')
+            .send('name=test')
+            .send('code=test')
+            .send('note=01')
+            .then(function(response){
+                expect(response.text).to.contain('true');
+            })
+    });
+});
+// describe('Unit testing the DELETE /api/episodes/:uuid', function() {
+//
+//     fs.writeFileSync(__dirname + '/../episodes/aaaaaaa-392a-11e9-b29b-33e4e41f52ac.json','test');
+//     it('should return OK status', function() {
+//         return request(app)
+//             .delete('/api/episodes/aaaaaaa-392a-11e9-b29b-33e4e41f52ac')
+//             .then(function(response){
+//                 assert.equal(response.status, 200)
+//             })
+//     });
+//
+// });
